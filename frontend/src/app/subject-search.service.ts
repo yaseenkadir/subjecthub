7ccc40b74ea5875632023f6924edfd5a62a6bdda
Miles <mjarrettjohnson@gmail.com>
2017-08-30 03:15:41 +1000

    Implemented an alternative search bar using fuse.js

--- frontend/src/app/subject-search.service.ts
+++ frontend/src/app/subject-search.service.ts
@@ -3,7 +3,7 @@ import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-
+import 'rxjs/add/operator/toPromise';
 import { SubjectResult } from './subject-result';
 
 @Injectable()
@@ -18,5 +18,12 @@ export class SubjectSearchService {
     return this.http
       .get(`api/subjects/?${selectedRoute}=${term}`)
       .map(response => response.json().data as SubjectResult[]);
+  }
+
+  fetch(): Promise<SubjectResult[]> {
+    return this.http
+      .get('api/subjects')
+      .toPromise()
+      .then(response => response.json().data as SubjectResult[]);
   }
 }
