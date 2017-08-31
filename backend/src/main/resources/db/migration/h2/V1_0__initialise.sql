0850c7ab5ff3a947dac260148cc57506b0a181ec
shuangjie_zhu <farseer7989@gmail.com>
2017-08-31 13:01:34 +0800

    Added two subjects

--- backend/src/main/resources/db/migration/h2/V1_0__initialise.sql
+++ backend/src/main/resources/db/migration/h2/V1_0__initialise.sql
@@ -102,3 +102,39 @@ VALUES (
     FALSE,
     1
 );
+
+/* .NET Application Development*/
+INSERT INTO subjects (subject_id, code, name, credit_points, description, min_requirements, undergrad,
+                      postgrad, autumn, spring, summer, faculty_id)
+VALUES (
+    3,
+    '32998',
+    '.NET Application Development',
+    6,
+    'This subject introduces C#, Visual Studio and the .NET development environment. The emphasis is on examining the .NET framework and the practicalities of developing software in this setting using the C# language.',
+    'In order to pass the subject you must attain all of the following minimum requirements /n 1.Minimum of 50% in the final exam /n 2.Minimum of 50% in the overall mark /n If you fail to attain requirement 1 but attain requirement 2 then your final grade will be set to X (fail). Otherwise, your final grade will be determined by your overall mark.',
+    FALSE,
+    TRUE,
+    FALSE,
+    TRUE,
+    FALSE,
+    1
+);
+
+/*  .NET Enterprise Development*/
+INSERT INTO subjects (subject_id, code, name, credit_points, description, min_requirements, undergrad,
+                      postgrad, autumn, spring, summer, faculty_id)
+VALUES (
+    4,
+    '32013',
+    '.NET Enterprise Development',
+    6,
+    'In this subject, students design, implement and then test web-based applications suitable for deployment in a modern enterprise. Students work in groups and gain experiences in the tools and practices used by small software development teams working in industry and embedded in larger organisations. The subject spans technical knowledge as well as the professional practices required by working software developers./n The subject is primarily focused on developing web-based, database-driven enterprise applications using the .NET framework. Technologies covered include LINQ, ASP.NET Web Forms, ASP.NET MVC, ADO.NET, Entity Framework and Identity. Practices include test-driven development, architectural layering and agile development. The language used is C#.',
+    'Students must achieve a mark of 50% overall to pass the subject. In addition, students must pass the final examination to pass the subject. That is, a student who obtains a result in the final exam that is less than 50%, will receive their overall mark with a failing grade (''X'')./nAssignments in this subject are done in groups. The contribution of each individual in a group will be monitored throughout the semester. Should the need arise, the mark allocated for individuals within groups can reflect an agreed level of contribution by members of the group. If no agreement can be reached or if the mark allocation does not properly reflect individual contribution or understanding, the tutor or subject coordinator may vary the individual marks to ensure an accurate measure of student learning./nA late penalty may be applied to submitted work unless prior arrangements have been made with the subject coordinator. Details of the late penalties will be included with the descriptions of the assessment items./nAssignments are due in labs on the date specified and should be submitted (and/or demonstrated) to your tutor, who will give you a receipt.',
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    FALSE,
+    1
+);
