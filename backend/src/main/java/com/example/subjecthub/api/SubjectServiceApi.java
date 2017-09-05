4a761d1ab0c4f00e58e15b164225722449ffa14b
Yaseen Kadir <yaseenkadir@ymail.com>
2017-09-05 23:51:37 +1000

    Fixed universities endpoint to follow hierarchical structure

--- backend/src/main/java/com/example/subjecthub/api/SubjectServiceApi.java
+++ backend/src/main/java/com/example/subjecthub/api/SubjectServiceApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@RequestMapping("/api/universities/{universityId}/subjects")
+@RequestMapping("/api/universities/university/{universityId}/subjects")
 @CrossOrigin(origins = "http://localhost:4200")
 public interface SubjectServiceApi {
 
