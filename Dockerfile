94334c21a2a4d1ebcaac7485dcc04a4dc70ae928
Yaseen Kadir <yaseenkadir@ymail.com>
2017-09-10 17:47:15 +1000

    Supplying keystore when container starts instead of baking it into jar
    - Reading secrets as ENV variables when running docker and unsetting them ASAP
    - Mounting certs directory to docker container
    - Removed baked in keystore password

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ADD backend/target/backend-0.0.1-SNAPSHOT.jar app.jar
-ENV JAVA_OPTS=""
 EXPOSE 8443
-ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar -Dspring.profiles.active=prod /app.jar"]
+ENTRYPOINT exec java -Djava.security.egd=file:/dev/./urandom \
+    -Dspring.profiles.active=prod \
+    -Dserver.ssl.key-store-password=$KEYSTORE_PASSWORD \
+    -jar /app.jar
--- run-new-version.sh
+++ run-new-version.sh
@@ -2,17 +2,25 @@
 # This is the script that is installed on the server. Changing it will do nothing. If you need to
 # make changes to the script let Yaseen know and he can do it.
 
-DOCKER_USERNAME=$1
-DOCKER_PASSWORD=$2
+source ~/.subject-hub/secrets.sh
 IMAGE_NAME="subjecthub-backend"
 REPO="yaseenk/subjecthub"
 TAG="latest"
 
 echo -e "Stopping image if it exists. No need to fret if a warning comes up."
 docker stop $IMAGE_NAME || true && docker rm $IMAGE_NAME || true
-echo -e "\n\nLogging in"
+echo -e "\nLogging in"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-echo -e "\n\nPulling latest changes"
+unset DOCKER_USERNAME
+unset DOCKER_PASSWORD
+echo -e "\nPulling latest changes"
 docker pull $REPO:$TAG
-echo -e "\n\nRunning new version of backed on port 80"
-docker run -d -p 443:8443 --name $IMAGE_NAME $REPO:$TAG
+echo -e "\nRunning new version of backend on port 80 and 443"
+docker run \
+    -v /etc/letsencrypt/:/etc/letsencrypt/ \
+    -e KEYSTORE_PASSWORD="$KEYSTORE_PASSWORD" \
+    -d \
+    -p 80:8443 \
+    -p 443:8443 \
+    --name $IMAGE_NAME $REPO:$TAG
+unset KEYSTORE_PASSWORD
