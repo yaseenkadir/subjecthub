d97c85a33d6e083fbef06e471b5972ee7aa52e06
Yaseen Kadir <yaseen95@users.noreply.github.com>
2017-09-13 22:30:58 +1000

    Added SubjectHubUser entity schema, class, and repository definition

---
+++ backend/src/main/java/com/example/subjecthub/repository/SubjectHubUserRepository.java
@@ -0,0 +1,13 @@
+package com.example.subjecthub.repository;
+
+import com.example.subjecthub.entity.SubjectHubUser;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface SubjectHubUserRepository extends CrudRepository<SubjectHubUser, Long> {
+
+    Optional<SubjectHubUser> findByUsername(String username);
+
+    Optional<SubjectHubUser> findByEmail(String email);
+}
