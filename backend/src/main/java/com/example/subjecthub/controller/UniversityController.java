780cb9436f47d6ff09c1cdd1d23f7653a2a8ddc8
FarahWheelbarrah <andrefarah5@gmail.com>
2017-09-21 15:00:41 +1000

    Added University API and fixed Faculty Json Loop

---
+++ backend/src/main/java/com/example/subjecthub/controller/UniversityController.java
@@ -0,0 +1,33 @@
+package com.example.subjecthub.controller;
+
+import com.example.subjecthub.api.UniversityServiceApi;
+import com.example.subjecthub.entity.University;
+import com.example.subjecthub.repository.UniversityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@RestController
+@ParametersAreNonnullByDefault
+@CrossOrigin(origins = "http://localhost:4200")
+public class UniversityController implements UniversityServiceApi {
+
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    @Override
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<University> getUniversities(@RequestParam(required = false) String abbreviation) {
+        return universityRepository.findAll();
+    }
+
+    @Override
+    @RequestMapping(value = "university/{universityId}", method = RequestMethod.GET)
+    public University getUniversity(
+        @PathVariable Long universityId
+    ) {
+        return universityRepository.findOne(universityId);
+    }
+}
