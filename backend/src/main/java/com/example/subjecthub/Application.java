a477148cb8429569f068e06c19c4caf2e1ce387f
Yaseen Kadir <yaseen95@users.noreply.github.com>
2017-09-22 23:10:42 +1000

    Added exipry to JWTs
    - Set app wide timeone to use UTC by default
    - Changed JwtTokenUtils secret key to bytes[]
    - Updated existing auth test and added new expired jwt test

--- backend/src/main/java/com/example/subjecthub/Application.java
+++ backend/src/main/java/com/example/subjecthub/Application.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
     public static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Application.class, args);
     }
 }
--- backend/src/test/java/com/example/subjecthub/testutils/TestUtils.java
+++ backend/src/test/java/com/example/subjecthub/testutils/TestUtils.java
@@ -1,13 +1,23 @@
 package com.example.subjecthub.testutils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
 
 public class TestUtils {
 
     public static String asJson(Object o) throws JsonProcessingException {
         ObjectWriter writer = new ObjectMapper().writer();
         return writer.writeValueAsString(o);
+    }
+
+    public static JsonNode fromString(String s) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readTree(s);
     }
 }
