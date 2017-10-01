7ff5c9c1440398349bef9059753abef2b8174127
Simran Bagga <simransbagga@gmail.com>
2017-10-02 10:01:42 +1100

    app, tags closed; service, api errors captured; css, styles from inline added; component, fixed according to review

--- frontend/src/app/services/subject-comments.service.ts
+++ frontend/src/app/services/subject-comments.service.ts
@@ -1,46 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/share';
+import {environment} from '../../environments/environment';
 import { SubjectComment } from '../models/subject-comment';
+import {SubjectHubApiResponse} from "../models/subject-hub-api-response";
 
 @Injectable()
 export class SubjectCommentsService {
     constructor(private http: Http) {}
 
-    fetch(university: number, subject: number): Promise<SubjectComment[]>{
-        return this.commentsAction(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments`,
+    fetch(university: number, subject: number): Promise<SubjectHubApiResponse>{
+        return this.commentsAction(`/universities/university/${university}/subjects/subject/${subject}/comments`,
             `No comments found for subject id: ${subject}`,`Comments request failed`);
     }
 
-    add(university: number, subject: number, message: string): Promise<SubjectComment>{
+    add(university: number, subject: number, message: string): Promise<SubjectHubApiResponse>{
         let requestBody = {"message": message};
-        return this.commentActionPost(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments/comment/add`,
+        return this.commentActionPost(`/universities/university/${university}/subjects/subject/${subject}/comments/comment/add`,
             requestBody,`Unable to add comment for subject id: ${subject}`,`Add comment request failed`)
     }
 
-    addThumbUp(university: number, subject: number, comment: number): Promise<SubjectComment>{
-        return this.commentAction(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/addThumbUp`,
+    addThumbUp(university: number, subject: number, comment: number): Promise<SubjectHubApiResponse>{
+        return this.commentAction(`/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/addThumbUp`,
             `Unable to add thumb up to comment (id:${comment}) for subject id: ${subject}`,
             `Add thumb up to comment (id:${comment}) request failed`)
     }
 
-    addThumbDown(university: number, subject: number, comment: number): Promise<SubjectComment>{
-        return this.commentAction(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/addThumbDown`,
+    addThumbDown(university: number, subject: number, comment: number): Promise<SubjectHubApiResponse>{
+        return this.commentAction(`/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/addThumbDown`,
             `Unable to add thumb down to comment (id:${comment}) for subject id: ${subject}`,
             `Add thumb down to comment (id:${comment}) request failed.`)
     }
 
-    flag(university: number, subject: number, comment: number): Promise<SubjectComment>{
-        return this.commentAction(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/flag`,
+    flag(university: number, subject: number, comment: number): Promise<SubjectHubApiResponse>{
+        return this.commentAction(`/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/flag`,
             `Unable to flag comment (id:${comment}) for subject id: ${subject}`, `Flag comment (id:${comment}) request failed.`)
     }
 
-    unFlag(university: number, subject: number, comment: number): Promise<SubjectComment>{
-        return this.commentAction(`http://localhost:8080/api/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/unflag`,
+    unFlag(university: number, subject: number, comment: number): Promise<SubjectHubApiResponse>{
+        return this.commentAction(`/universities/university/${university}/subjects/subject/${subject}/comments/comment/${comment}/unflag`,
             `Unable to unflag comment (id:${comment}) for subject id: ${subject}`,`Unflag comment (id:${comment}) request failed.`)
     }
 
@@ -51,21 +52,21 @@ export class SubjectCommentsService {
      * @param {string} errorMessageRequest
      * @returns {Promise<SubjectComment[]>}
      */
-    commentsAction(url: string, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectComment[]>{
+    commentsAction(url: string, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectHubApiResponse>{
         return this.http
-            .get(url)
+            .get(environment.API_URL + url)
             .toPromise()
             .then(response => {
                 if(response.status == 200){
-                    return response.json() as SubjectComment[];
+                    return new SubjectHubApiResponse(true, response.json() as SubjectComment[]);
                 } else {
                     console.log(errorMessageApi);
-                    return null;
+                    return new SubjectHubApiResponse(false, response.json());
                 }
             })
             .catch(e => {
                 console.log(`${errorMessageRequest} Caught exception: ${e}`);
-                return null;
+                return new SubjectHubApiResponse(false, e.json());
             });
     }
 
@@ -76,21 +77,21 @@ export class SubjectCommentsService {
      * @param {string} errorMessageRequest
      * @returns {Promise<SubjectComment>}
      */
-    commentAction(url: string, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectComment>{
+    commentAction(url: string, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectHubApiResponse>{
         return this.http
-            .get(url)
+            .get(environment.API_URL + url)
             .toPromise()
             .then(response => {
                 if(response.status == 200){
-                    return response.json() as SubjectComment;
+                    return new SubjectHubApiResponse(true, response.json() as SubjectComment);
                 } else {
                     console.log(errorMessageApi);
-                    return null;
+                    return new SubjectHubApiResponse(false, response.json());
                 }
             })
             .catch(e => {
                 console.log(`${errorMessageRequest} Caught exception: ${e}`);
-                return null;
+                return new SubjectHubApiResponse(false, e.json());
             });
     }
 
@@ -102,21 +103,21 @@ export class SubjectCommentsService {
      * @param {string} errorMessageRequest
      * @returns {Promise<SubjectComment>}
      */
-    commentActionPost(url: string, requestBody: object, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectComment>{
+    commentActionPost(url: string, requestBody: object, errorMessageApi: string, errorMessageRequest: string): Promise<SubjectHubApiResponse>{
         return this.http
-            .post(url,requestBody)
+            .post(environment.API_URL + url, requestBody)
             .toPromise()
             .then(response => {
                 if(response.status == 200){
-                    return response.json() as SubjectComment;
+                    return new SubjectHubApiResponse(true, response.json() as SubjectComment);
                 } else {
                     console.log(errorMessageApi);
-                    return null;
+                    return new SubjectHubApiResponse(false, response.json());
                 }
             })
             .catch(e => {
                 console.log(`${errorMessageRequest} Caught exception: ${e}`);
-                return null;
+                return new SubjectHubApiResponse(false, e.json());
             });
     }
 }
--- frontend/src/app/subject-comments/subject-comments.component.ts
+++ frontend/src/app/subject-comments/subject-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SubjectCommentsService} from "../services/subject-comments.service";
 import {SubjectComment} from "../models/subject-comment";
+import {forEachComment} from "tslint";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-subject-comments',
@@ -13,33 +15,37 @@ export class SubjectCommentsComponent implements OnInit {
 
   comments: SubjectComment[];
   fatalMessage: string = null;
+  sysErrorMessageTemplate: string = "Comments system has encountered an error, please refresh.";
   message: string = null;
   canAddComment: boolean = false;
   universityId: number = 1; //to be provided by parent in the future
   subjectId: number = 1; //to be provided by parent in the future
 
   constructor(private subjectCommentsService: SubjectCommentsService) {
-      this.fetch();
+
   }
 
-  ngOnInit() {  }
+  ngOnInit() {
+      this.fetch();
+  }
 
   fetch(){
       this.fatalMessage = null;
       this.canAddComment = false;
       this.subjectCommentsService.fetch(this.universityId, this.subjectId)
           .then(result => {
-              if(result.length > 0){
-                  this.comments = result;
+              if(result.isSuccessful()){
+                  this.comments = result.response;
               }else{
-                  this.fatalMessage = "No comments found.";
+                  this.fatalMessage = result.response['message'];
               }
               if(this.isAuth()) {
                   this.canAddComment = true;
               }
           })
           .catch(e => {
-              this.fatalMessage = "Comments system has encountered an error, please refresh.";
+              this.fatalMessage = this.sysErrorMessageTemplate;
+              console.log(e)
               this.canAddComment = false;
           });
   }
@@ -49,81 +55,95 @@ export class SubjectCommentsComponent implements OnInit {
       this.canAddComment = true;
   }
 
-  thumbUp(commentId){
+  thumbUp(commentId: number){
       this.message = null;
-      this.subjectCommentsService.addThumbUp(this.universityId,this.subjectId,commentId)
+      this.subjectCommentsService.addThumbUp(this.universityId, this.subjectId, commentId)
           .then(result => {
-              if(result){
-                  this.fetch();
+              if(result.isSuccessful()){
+                  this.placeComment(result.response);
               }else{
-                  this.message = "Unable to find comment, please refresh."
+                  this.message = result.response['message'];
               }
           })
           .catch(e => {
-              this.message = "Comments system has encountered an error, please refresh."
+              this.message = this.sysErrorMessageTemplate;
+              console.log(e)
           });
   }
 
-    thumbDown(commentId){
+    thumbDown(commentId: number){
         this.message = null;
-        this.subjectCommentsService.addThumbDown(this.universityId,this.subjectId,commentId)
+        this.subjectCommentsService.addThumbDown(this.universityId, this.subjectId, commentId)
             .then(result => {
-                if(result){
-                    this.fetch();
+                if(result.isSuccessful()){
+                    this.placeComment(result.response)
                 }else{
-                    this.message = "Unable to find comment, please refresh."
+                    this.message = result.response['message'];
                 }
             })
             .catch(e => {
-                this.message = "Comments system has encountered an error, please refresh."
+                this.message = this.sysErrorMessageTemplate;
             });
     }
 
-    flag(commentId){
+    flag(commentId: number){
         this.message = null;
-        this.subjectCommentsService.flag(this.universityId,this.subjectId,commentId)
+        this.subjectCommentsService.flag(this.universityId, this.subjectId, commentId)
             .then(result => {
-                if(result){
-                    this.fetch();
+                if(result.isSuccessful()){
+                    this.placeComment(result.response)
                 }else{
-                    this.message = "Unable to find comment, please refresh."
+                    this.message = result.response['message'];
                 }
             })
             .catch(e => {
-                this.message = "Comments system has encountered an error, please refresh."
+                this.message = this.sysErrorMessageTemplate;
             });
     }
 
-    unflag(commentId){
+    unflag(commentId: number){
         this.message = null;
-        this.subjectCommentsService.unFlag(this.universityId,this.subjectId,commentId)
+        this.subjectCommentsService.unFlag(this.universityId, this.subjectId, commentId)
             .then(result => {
-                if(result){
-                    this.fetch();
+                if(result.isSuccessful()){
+                    this.placeComment(result.response)
                 }else{
-                    this.message = "Unable to find comment, please refresh."
+                    this.message = result.response['message'];
                 }
             })
             .catch(e => {
-                this.message = "Comments system has encountered an error, please refresh."
+                this.message = this.sysErrorMessageTemplate;
             });
     }
 
-    addNew(message){
+    addComment(message: string){
         this.message = null;
         //need auth? or is spring enough
         if(this.canAddComment){
-            this.subjectCommentsService.add(this.universityId,this.subjectId, message)
+            this.subjectCommentsService.add(this.universityId, this.subjectId, message)
                 .then(result => {
-                    if(result){
-                        this.fetch();
+                    if(result.isSuccessful()){
+                        this.placeComment(result.response)
                     }else{
-                        this.message = "Unable to find subject, please refresh."
+                        this.message = result.response['message'];
                     }
                 })
                 .catch(e => {
-                    this.message = "Comments system has encountered an error, please refresh or login."
+                    this.message = this.sysErrorMessageTemplate.replace("."," ") + "or login.";
                 });
+        }
+    }
+
+    placeComment(response: SubjectComment){
+        let found : boolean = false;
+        this.comments.forEach(currentValue => {
+            if(currentValue.id = response.id){
+                currentValue = response;
+                found = true;
+            }
+        });
+        if (!found){
+            this.comments.push(response);
         }
     }
 
