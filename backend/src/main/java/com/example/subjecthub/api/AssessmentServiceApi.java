2dee29fbbd95d82379c1f8a8b922e49e1949a4b0
Yaseen Kadir <yaseenkadir@ymail.com>
2017-10-09 09:33:07 +1100

    Removed controller specific concerns from AssessmentServiceApi

--- backend/src/main/java/com/example/subjecthub/api/AssessmentServiceApi.java
+++ backend/src/main/java/com/example/subjecthub/api/AssessmentServiceApi.java
@@ -2,35 +2,21 @@ package com.example.subjecthub.api;
 
 import com.example.subjecthub.entity.Assessment;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
-@RequestMapping("/api/universities/university/{universityId}/subjects/subject/{subjectId}/assessments")
-@CrossOrigin(origins = "http://localhost:4200")
+@ParametersAreNonnullByDefault
 public interface AssessmentServiceApi {
 
-    // get all assessments under a subject related to a university, with path parameters of universityId & subjectId
-    // assessment type and/or weighting and/or group work can be used as a filter to find the specific assessment
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public List<Assessment> getAssessments(
-        @PathVariable Long universityId,
-        @PathVariable Long subjectId,
-        @RequestParam(required = false) Assessment.AssessmentType type,
-        @RequestParam(required = false) Integer weighting,
-        @RequestParam(required = false) Boolean groupWork,
-        @RequestParam(required = false) String name
+    List<Assessment> getAssessments(
+        Long universityId,
+        Long subjectId,
+        @Nullable Assessment.AssessmentType type,
+        @Nullable Integer weighting,
+        @Nullable Boolean groupWork,
+        @Nullable String name
     );
 
-    //get a single assessment with assessmentId
-    @RequestMapping(value = "/assessment/{assessmentId}", method = RequestMethod.GET)
-    public Assessment getAssessment(
-        @PathVariable Long universityId,
-        @PathVariable Long subjectId,
-        @PathVariable Long assessmentId
-    );
+    Assessment getAssessment(Long universityId, Long subjectId, Long assessmentId);
 }
--- backend/src/main/java/com/example/subjecthub/controller/AssessmentServiceController.java
+++ backend/src/main/java/com/example/subjecthub/controller/AssessmentServiceController.java
@@ -1,62 +1,84 @@
 package com.example.subjecthub.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-
-import com.example.subjecthub.utils.FuzzyUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.subjecthub.Application;
 import com.example.subjecthub.api.AssessmentServiceApi;
 import com.example.subjecthub.entity.Assessment;
 import com.example.subjecthub.repository.AssessmentRepository;
+import com.example.subjecthub.utils.FuzzyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @ParametersAreNonnullByDefault
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/universities/university/{universityId}/subjects/subject/{subjectId}/assessments")
 public class AssessmentServiceController implements AssessmentServiceApi {
 
-	@Autowired
-	private AssessmentRepository assessmentRepository;
+    @Autowired
+    private AssessmentRepository assessmentRepository;
 
-	// business logic of get all assessments
-	@Override
-	public List<Assessment> getAssessments(
-			@PathVariable Long universityId,
-			@PathVariable Long subjectId,
-			@RequestParam(required = false) Assessment.AssessmentType type,
-			@RequestParam(required = false) Integer weighting,
-			@RequestParam(required = false) Boolean groupWork,
-            @RequestParam(required = false) String name
-			) {
+    /**
+     * Gets all assessments for a specific university and subject. If optional params are non null,
+     * they will be used to filter results.
+     * @param universityId
+     * @param subjectId
+     * @param type - Optional, filter by type
+     * @param weighting - Optional, filter by weighting
+     * @param groupWork - Optional, filter by whether it has groupWork or not
+     * @param name - Optional, filter by name
+     * @return
+     */
+    @Override
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Assessment> getAssessments(
+        @PathVariable Long universityId,
+        @PathVariable Long subjectId,
+        @RequestParam(required = false) @Nullable Assessment.AssessmentType type,
+        @RequestParam(required = false) @Nullable Integer weighting,
+        @RequestParam(required = false) @Nullable Boolean groupWork,
+        @RequestParam(required = false) @Nullable String name
+    ) {
 
-		Application.log.info("Received:\n type: {}\n weighting: {}\n groupWork: {}\n name: {}\n universityId: {}\n subjectId: {}",
+        Application.log.info("Received:\n type: {}\n weighting: {}\n groupWork: {}\n name: {}\n universityId: {}\n subjectId: {}",
             type, weighting, groupWork, name, universityId, subjectId);
 
-		// filter to compare the type / weighting / group work in Assessment entity with the value passed in the request parameter
-        // then return the assessment list match the criteria
-		return assessmentRepository.findBySubjectId(subjectId).stream()
-                .filter(s -> (type == null|| s.getType() == type))
-				.filter(s -> (weighting == null || s.getWeighting() == weighting))
-				.filter(s -> (groupWork == null || s.isGroupWork() == groupWork))
-                .filter(s -> (name == null || FuzzyUtils.isSimilar(s.getName(), name)))
-				.collect(Collectors.toList());
-	}
+        // filter to compare the type / weighting / group work in Assessment entity with the value
+        // passed in the request parameter then return the assessment list match the criteria
+        return assessmentRepository.findBySubjectId(subjectId).stream()
+            .filter(s -> (type == null || s.getType() == type))
+            .filter(s -> (weighting == null || s.getWeighting() == weighting))
+            .filter(s -> (groupWork == null || s.isGroupWork() == groupWork))
+            .filter(s -> (name == null || FuzzyUtils.isSimilar(s.getName(), name)))
+            .collect(Collectors.toList());
+    }
 
-	// find a single assessment by its assessment id
-	@Override
-	public Assessment getAssessment(
-			@PathVariable Long universityId,
-			@PathVariable Long subjectId,
-			@PathVariable Long assessmentId
-			) {
-	    //To avoid cross university & subject fetching later
-		return assessmentRepository.findOne(assessmentId);
-	}
+    //
+
+    /**
+     * Find a single assessment by its assessment id
+     * @param universityId
+     * @param subjectId
+     * @param assessmentId
+     * @return
+     */
+    @Override
+    @RequestMapping(value = "/assessment/{assessmentId}", method = RequestMethod.GET)
+    public Assessment getAssessment(
+        @PathVariable Long universityId,
+        @PathVariable Long subjectId,
+        @PathVariable Long assessmentId
+    ) {
+        //To avoid cross university & subject fetching later
+        return assessmentRepository.findOne(assessmentId);
+    }
 }
