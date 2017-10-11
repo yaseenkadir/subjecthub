355260b92e1a36fbd1e033c1f9b2c142d5116dbb
Andre Farah <andrefarah5@gmail.com>
2017-10-11 13:43:27 +1100

    Subject Additions (#58)
    * Added subjects to both UTS and UOW. Also, made up faculty codes for UOW faculties
    
    * Removed made-up faculty codes for UOW

--- backend/src/main/resources/data-h2.sql
+++ backend/src/main/resources/data-h2.sql
@@ -247,6 +247,60 @@ VALUES (
     1
 );
 
+/* Data Structures and Algorithms */
+INSERT INTO subjects (subject_id, code, name, credit_points, description, min_requirements,
+                      undergrad, postgrad, autumn, spring, summer, faculty_id)
+VALUES (
+    9,
+    '31251',
+    'Data Structures and Algorithms',
+    6,
+    'This subject teaches students how to design, develop and evaluate data structures and algorithms to meet predefined quality characteristics of functionality (suitability) and usability (understandability, learnability, operability, compliance). Software solutions are implemented using C++. Concepts, theories and technologies underlying the methods and techniques are introduced and explained as required.',
+    'In order to pass the subject you must attain all of the following minimum requirements\n1. Minimum of 16 out of 40 (40%) in the final exam.\n2. Minimum of 50 out of 100 (50%) in the overall mark.\nIf you fail to achieve the first requirement but meet the second then your final grade will be set to X (fail)',
+    TRUE,
+    FALSE,
+    TRUE,
+    FALSE,
+    FALSE,
+    1
+);
+
+/* Introduction to Health Care Systems */
+INSERT INTO subjects (subject_id, code, name, credit_points, description, min_requirements,
+                      undergrad, postgrad, autumn, spring, summer, faculty_id)
+VALUES (
+    10,
+    '92566',
+    'Introduction to Health Care Systems',
+    6,
+    'This subject introduces students to the major structural and functional components of healthcare systems, both in Australia and overseas. Key arrangements, stakeholders, processes and performance and trends in health system reform are actively explored. Students develop the ability to critically evaluate health systems, skills to identify and prioritise health system needs and identify and plan strategies that address major challenges. This subject provides a foundation for students to influence health system improvement, from an informed position, in health organisations.',
+    'A student must achieve an overall mark of 50% or greater in the subject.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    2
+);
+
+/* Foundations of Law */
+INSERT INTO subjects (subject_id, code, name, credit_points, description, min_requirements,
+                      undergrad, postgrad, autumn, spring, summer, faculty_id)
+VALUES (
+    11,
+    '70102',
+    'Foundations of Law',
+    8,
+    'This subject introduces students to the foundations of Australian law: its origins, institutions, cultural contexts and theoretical foundations. We examine the role of the legal doctrine of terra nullius in the British colonisation of Australian peoples and places, and its connection to the ‘reception’ of English law into Australia. We explore the key ideas that underpin Australia’s legal institutions including democracy, sovereignty, the rule of law and the separation of powers. We combine our understanding of the historical development of Australian law and legal institutions with a critical analysis of their conceptual underpinnings using critical legal theory. This theory scrutinises the assumptions, logic, language and practice of law. Using a critical analysis of law, for example, from the perspective of the colonised rather than the colonising, allows students to ask different questions about not only the abstract principles of law, but also the lived experience of law. The subject also introduces students to the nature of legal thinking and legal practice including research methods, and the techniques and principles involved in reading and interpreting case law and statute. Legal reasoning is one of the most important topics in the subject and students are given the opportunity to explore both traditional methods of legal reasoning and critical lenses through which to analyse and evaluate a legal question. For instance, using feminist legal theories and critical race theory we can arrive at different answers to the same legal question. The critical legal thinking and legal research skills that students develop in this subject are essential to the successful completion of later subjects in the law degree program.',
+    'Class attendance is a subject requirement. Your seminar leader will take a roll during class. Although we encourage you to attend every seminar we understand that things happen and you can afford to miss up to four seminars without penalty provided that you send an email to your seminar leader to explain your absence. However, if you miss more than four classes you will not be permitted to receive a mark for the subject. In exceptional circumstances of illness and misadventure, you may be offered an opportunity to submit additional work for each class missed and permitted to receive a mark for the subject. Please note that work commitments and personal travel do not constitute an acceptable reason for absence from seminars. Please do not plan holidays that coincide with UTS teaching weeks.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    3
+);
+
 
 /*
   _   _    ___   __        __
@@ -266,6 +320,7 @@ INSERT INTO faculties (faculty_id, name, code, university_id)
 VALUES (5, 'Engineering and Information Sciences', NULL, 2);
 
 /* Subjects */
+/* Programming Fundamentals */
 INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
                       summer, faculty_id)
 VALUES (
@@ -281,6 +336,7 @@ VALUES (
     5
 );
 
+/* Introduction to Web Technology */
 INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
                       summer, faculty_id)
 VALUES (
@@ -294,4 +350,148 @@ VALUES (
     TRUE,
     FALSE,
     5
+);
+
+/* Data Management & Security */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'CSIT115',
+    'Data Management & Security',
+    6,
+    'The subject investigates three major areas of modern data management systems: data modelling, data processing, and data security. The goal of the subject is to learn the fundamental concepts in data management including conceptual modelling, the relational data model, processing of relational data with Structured Query Language (SQL), enforcing the concepts of data confidentiality, integrity, and availability data management systems. The subject develops the skills in the design, implementation, processing, and security of data management systems. The subject covers the following topics in data security: discretionary access control, user management, enforcing data security and integrity. The subject also explains the important ethical issues associated with responsible disclosure, responsibility, liability, security weaknesses, and privacy in data management systems.',
+    TRUE,
+    FALSE,
+    TRUE,
+    FALSE,
+    FALSE,
+    5
+);
+
+/* Problem Solving */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'CSIT113',
+    'Problem Solving',
+    6,
+    'This subject introduces the analysis of problems and the strategies used to manage them, primarily in the context of computing. Problem classification is introduced, as are formal and informal approaches to problem solving. The importance of method and method classification for problem solving strategies is motivated, and the need to compare and analyse strategies is justified. Introductory tools for the analysis of strategies are covered. Appropriate representations for problem solving are explored.',
+    TRUE,
+    FALSE,
+    TRUE,
+    FALSE,
+    FALSE,
+    5
+);
+
+/* Algorithms and Data Structures */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'CSCI203',
+    'Algorithms and Data Structures',
+    6,
+    'Approaches to analysing algorithm complexity, introduced in first year subjects, will be reviewed. The use of abstract data types as a design technique, and their implementation in solutions to problems, will form a large part of the subject. The concept of efficient code and ways to measure efficiency (both empirically, by timings, and theoretically) will be studied.',
+    TRUE,
+    FALSE,
+    FALSE,
+    TRUE,
+    FALSE,
+    5
+);
+
+/* Human Computer Interaction */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'CSIT226',
+    'Human Computer Interaction',
+    6,
+    'The subject provides students with an understanding of Human Computer Interaction (HCI) principles and practices, and how to apply them in the context of developing usable interactive computer applications and systems. The subject also emphasises the importance of taking into account contextual, organisational, and social factors in the design of computer systems. Students will be taken through the analysis, design, development, and evaluation of user interfaces. They will acquire hands-on design skills through an interaction design project. The subject will cover topics including user-centred design, the development process, prototyping, usability testing, measuring and evaluating the user experience and accessibility.',
+    TRUE,
+    FALSE,
+    FALSE,
+    TRUE,
+    FALSE,
+    5
+);
+
+/* Principles of Responsible Commerce */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'COMM101',
+    'Principles of Responsible Commerce',
+    6,
+    'The subject provides students with a conceptual tool kit for understanding and practising responsible and ethical Commerce. The topics covered will include the origins of contemporary systems of commerce, ethical and social responsibility in commerce and developments in ethical and responsible commerce. Areas addressed include the environment, globalization, technology, anti-corruption, labour and human rights. Students will examine these issues from a variety of theoretical and practical perspectives and apply them to contemporary commercial contexts.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    4
+);
+
+/* Business Oriented Information Systems */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'COMM113',
+    'Business Oriented Information Systems',
+    6,
+    'Information systems (IS) form an integral part of modern organisations and are used to support all aspects of an organisation’s daily functions and activities. This subject introduces the fundamental information system concepts that facilitate business processes. It explores how organisations use information, IS and their respective applications to increase profitability, gain market share, improve customer service and manage daily operations whilst understanding the social implications of their decisions. Students will learn about the role of IS in the modern organisation and how IS supports all of the functional areas of an organisation – Accounting, Finance, Marketing, Human Resources and Production/Operations Management.',
+    TRUE,
+    FALSE,
+    FALSE,
+    TRUE,
+    FALSE,
+    4
+);
+
+/* Marketing Principles */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'MARK101',
+    'Marketing Principles',
+    6,
+    'Marketing is a set of activities and processes for creating, communicating and delivering offerings and facilitating satisfying exchange relationships in a way that delivers value for consumers and society. Organisations need to know how to define and segment a market and how to position themselves strongly by identifying marketing opportunities and problems, and developing products, services, experiences and ideas for chosen target markets more effectively than their competitors. Marketing is essential for all organisations including manufacturers, wholesalers, retailers, professional services firms including lawyers, accountants and architects, and non-profit institutions including charities and museums. The subject examines the fundamental concepts underpinning the marketing process and theories relevant to the study and practice of marketing. It serves as a foundation for further studies in business by developing an overview of where marketing fits within organisations and what framework marketing provides for enhancing and enabling the conduct of a business.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    4
+);
+
+/* Accounting Fundamentals in Society */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'ACCY111',
+    'Accounting Fundamentals in Society',
+    6,
+    'This subject introduces the role of accounting information in society including its social and ethical aspects relating to both the individual and the organisation. The subject introduces basic accounting language, concepts and techniques to identify, classify, process, record and present accounting and financial information. The subject also considers accounting information that can be used for making decisions about past and future economic events in a variety of business and social settings.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    4
+);
+
+/* Macroeconomic Essentials for Business */
+INSERT INTO subjects (code, name, credit_points, description, undergrad, postgrad, autumn, spring,
+                      summer, faculty_id)
+VALUES (
+    'ECON101',
+    'Macroeconomic Essentials for Business',
+    6,
+    'This subject analyses relevant macroeconomic concepts and principles in an integrated macroeconomic environment. Simple macroeconomic models will be developed to characterise the interdependencies of the more important components parts of a macro economy. This will allow students to analyse some real world problems and to start identifying and formulating appropriate macroeconomic policies.',
+    TRUE,
+    FALSE,
+    TRUE,
+    TRUE,
+    FALSE,
+    4
 );
