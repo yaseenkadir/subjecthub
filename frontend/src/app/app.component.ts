45e3573a0b6924693f921bb720e43dfb6db6d19d
Yaseen Kadir <yaseen95@users.noreply.github.com>
2017-10-23 23:48:07 +1100

    Displaying loading spinner during bundle download
    - Displaying spinner icon during bundle download
    - Moved spinner css into index.html css file
    - Moved font styling css into index.html
    - Hiding spinner once app loads

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'UTS Subject Hub';
+
+
+  ngOnInit(): void {
+    // Hide spinner that was displayed during bundle load
+    //
+    document.getElementById('loadingContainer').style.display = 'None';
+  }
 }
